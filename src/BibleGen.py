08bdd81c5ec2e7988beae8c5cd84ea8d3a3a2db9
SeanMcGrath <srmcgrat@umass.edu>
2015-04-04 17:11:42 -0400

    comand takes arbitrary number of input files

--- src/BibleGen.py
+++ src/BibleGen.py
@@ -6,31 +6,33 @@ import sys
 def main(args):
 
 	if not args:
-		print('Usage: BibleGen INFILE [OUTFILE]')
+		print('Usage: BibleGen INFILE(s)')
 		return 1
 	else:
-		try:
-			corpus = open(args[0], 'r')
-		except:
-			print(args[0] + ' is not a valid input file.')
-			return 1
+		corpus = []
+		for arg in args:
+			try:
+				corpus.append(open(arg, 'r'))
+			except Exception as e:
+				print(e)
 
-		try:
-			d = doc(r'../output/output.tex')
-			textGen = TextGenerator(corpus)
-			title = textGen.title()
-			print('\nBEHOLD:\n\n')
-			print('THE GODS HAVE SMILED UPON US THIS DAY\n\n')
-			print('THEY PASS ON TO YOU THIS HOLY TEXT:\n\n')
-			print(title + '\n\n')
-			print('SPREAD ITS MIGHTY WORD THROUGHOUT THE LAND')
-			d.begin(title)
-			for i in range(0, randint(10,20)):
-				d.writeChapter(textGen.title(),textGen.chapter())
-			d.compile()
-		except Exception as e:
-			print(e)
-			return 1
+		if corpus:
+			try:
+				d = doc(r'../output/output.tex')
+				textGen = TextGenerator(corpus)
+				title = textGen.title()
+				print('\nBEHOLD:\n\n')
+				print('THE GODS HAVE SMILED UPON US THIS DAY\n\n')
+				print('THEY PASS ON TO YOU THIS HOLY TEXT:\n\n')
+				print(title + '\n\n')
+				print('SPREAD ITS MIGHTY WORD THROUGHOUT THE LAND')
+				d.begin(title)
+				for i in range(0, randint(10,20)):
+					d.writeChapter(textGen.title(),textGen.chapter())
+				d.compile()
+			except Exception as e:
+				print(e)
+				return 1
 
 		return 0
 
